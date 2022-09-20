9b0bd3adfe610a32a8fbea4eab02df3e192ba750
fransoltero <114007407+fransoltero@users.noreply.github.com>
2022-09-20 18:04:52 +0200

    20/09/2022

---
+++ src/main/java/com/mycompany/programa/Padre.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.programa;
+
+/**
+ *
+ * @author fran2
+ */
+public class Padre {
+    
+    public void funcion1(){
+        System.out.println("Esta es una clase padre");
+    }
+    
+}
--- src/main/java/com/mycompany/programa/Programa.java
+++ src/main/java/com/mycompany/programa/Programa.java
@@ -13,5 +13,6 @@ public class Programa {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+            System.out.println("Bienvenidos");
     }
 }
